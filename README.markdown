28c4057a95090cf8fbbebae52eec4c2fc28602cf
Benjamin Blundell <me@benjamin.computer>
2020-01-29 14:46:27 +0000

    added process and lib to the mix

--- README.markdown
+++ README.markdown
@@ -14,3 +14,14 @@ If you have rust installed, enter the swiss_parse directory and type
 I think it's a good idea to make sure your rust compilier is up-to-date. I've fallen afoul of this a few times
 
     rustup upgrade
+
+## Process
+
+The program *explorer* views the tiff files and spits out fits format images. The program process does the same but a tad faster with no GTK window. The programs are invoked as follows:
+
+    cargo run --release --bin explorer <path to dora tiffs> <path to save fits>
+    cargo run --release --bin process <path to dora tiffs> <path to save fits> <num threads>
+
+### Image processing
+
+Dora's tiffs appear to be 3D; stacks of about 41 images long, in 16 bit greyscale. We change these to a mean average, adding up every pixel and dividing by the number of layers in the Z. We save as floating point as that's a bit easier to mess with further down the line.
---
+++ src/bin/process.rs
@@ -0,0 +1,143 @@
+/// A small program that lets us process
+/// a directory of Dora's images
+//
+///
+/// Author: Benjamin Blundell
+/// Email: me@benjamin.computer
+
+extern crate rand;
+extern crate image;
+extern crate nalgebra as na;
+extern crate probability;
+extern crate scoped_threadpool;
+extern crate ndarray;
+extern crate gtk;
+extern crate gio;
+extern crate gdk_pixbuf;
+extern crate glib;
+extern crate tiff;
+extern crate fitrs;
+
+use gtk::prelude::*;
+use gio::prelude::*;
+use gdk_pixbuf::Pixbuf;
+use gdk_pixbuf::Colorspace;
+use glib::Bytes;
+use glib::clone;
+
+use std::env;
+use std::fmt;
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io::prelude::*;
+use rand::prelude::*;
+use std::path::{Path, PathBuf};
+use std::sync::{Arc, Mutex};
+use std::{cell::Cell, rc::Rc, cell::RefCell};
+use tiff::decoder::{ifd, Decoder, DecodingResult};
+use tiff::ColorType;
+use std::process;
+use fitrs::{Fits, Hdu};
+use rand::distributions::Uniform;
+use scoped_threadpool::Pool;
+use std::sync::mpsc::channel;
+use pbr::ProgressBar;
+use ndarray::{Slice, SliceInfo, s, Array1};
+use gtk::{Application, ApplicationWindow, Button};
+use dora_explorer::dora_tiff::save_fits;
+use dora_explorer::dora_tiff::tiff_to_vec;
+
+static WIDTH : u32 = 128;
+static HEIGHT : u32 = 128;
+
+
+
+// render function. Breaks up the list of paths into chunks for each thread
+fn render (image_paths : &Vec<PathBuf>, out_path : &String,  nthreads : u32) {
+    // Split into threads here I think
+    let pi = std::f32::consts::PI;
+    let (tx, rx) = channel();
+    let mut progress : i32 = 0;
+    let mut pool = Pool::new(nthreads);
+
+    let num_runs = image_paths.len() as u32;
+    let truns = (num_runs / nthreads) as u32;
+    let spare = (num_runs % nthreads) as u32;
+    let mut pb = ProgressBar::new(num_runs as u64);
+    pb.format("╢▌▌░╟");
+
+    pool.scoped(|scoped| {
+        for _t in 0..nthreads {
+            let tx = tx.clone();
+            let start : usize = (_t * truns) as usize;
+            let mut end = ((_t + 1)  * truns) as usize;
+            if _t == nthreads - 1 { end = end + (spare as usize); }
+            let cslice = &image_paths[start..end];
+           
+            scoped.execute( move || { 
+                let mut rng = thread_rng();
+                let side = Uniform::new(-pi, pi);
+
+                for _i in 0..cslice.len() {
+                    let (timg, minp, maxp, levels) = tiff_to_vec(&cslice[_i], WIDTH as usize, HEIGHT as usize);
+                    let fidx = format!("/image_{:06}.fits", (start + _i) as usize);
+                    let mut fitspath = out_path.clone();
+                    fitspath.push_str(&fidx);
+                    save_fits(&timg, &fitspath, WIDTH as usize, HEIGHT as usize);
+                    tx.send(_i).unwrap();
+                }
+            });
+        }
+
+        // Update our progress bar
+        while progress < num_runs as i32 {
+            match rx.try_recv() {
+                Ok(_a) => {
+                    pb.inc();
+                    progress = progress + 1;
+                }, Err(_e) => {}
+            }
+        }
+    });
+}
+
+// Main entry point where we parse the command line, read in all the images
+// into a path buffer and set off all the threads.
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let mut image_files : Vec<PathBuf> = vec!();
+    
+    if args.len() < 4 {
+        println!("Usage: explorer <path to directory of tiff files> <output dir> <num threads>"); 
+        process::exit(1);
+    }
+
+    let paths = fs::read_dir(Path::new(&args[1])).unwrap();
+    let nthreads = args[3].parse::<u32>().unwrap();
+
+    for path in paths {
+        match path {
+            Ok(file) => {
+                let filename = file.file_name();
+                let tx = filename.to_str().unwrap();
+                if tx.contains("tif") {
+                    println!("Found tiff: {}", tx);
+
+                    let mut owned_string: String = args[1].to_owned();
+                    let borrowed_string: &str = "/";
+                    owned_string.push_str(borrowed_string);
+                    owned_string.push_str(&tx.to_string());
+                    image_files.push(PathBuf::from(owned_string));
+                }
+            },
+            Err(e) => {
+                println!("Error walking directory.");
+            }
+            
+        }
+       
+    }
+
+    render(&image_files, &args[2], nthreads);
+}
